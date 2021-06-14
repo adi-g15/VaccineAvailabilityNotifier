2528ec7d8675ccc92437451c8cc2a870fa9e28bb
Aditya Gupta <ag15035@gmail.com>
2021-06-15 01:07:32 +0530

    Lesser deps; Custom number of days

--- vaccineNotifier.js
+++ vaccineNotifier.js
@@ -1,7 +1,5 @@
-require('dotenv').config()
-const moment = require('moment');
-const cron = require('node-cron');
-const axios = require('axios');
+require('dotenv').config();
+const fetch = require('node-fetch');
 const notifier = require('./notifier');
 /**
 Step 1) Enable application access on your gmail with steps given here:
@@ -18,52 +16,61 @@ const PINCODE = process.env.PINCODE
 const EMAIL = process.env.EMAIL
 const AGE = process.env.AGE
 
+// This needs to be run every minute
 async function main(){
-    try {
-        cron.schedule('* * * * *', async () => {
-             await checkAvailability();
-        });
-    } catch (e) {
-        console.log('an error occured: ' + JSON.stringify(e, null, 2));
-        throw e;
-    }
+   return checkAvailability().then(slots => {
+	console.log("The following slots are available: ", slots);
+	const dates = Object.keys(slots).sort();
+	console.log("For dates: ", dates.join(', '));
+	// mail in batch here
+   });
 }
 
-async function checkAvailability() {
+function checkAvailability() {
+    let datesArray = fetchNDays(3);
+	console.log(datesArray.length, datesArray);
+    let slots = {};
+    let indices_checked = 0;
+    return new Promise((resolve) => {
+	datesArray.forEach(async (date,i) => {
+        	const valid_slots = await getSlotsForDate(date);
+		if( valid_slots.length > 0 ) {
+			slots[date] = valid_slots;
+		}
 
-    let datesArray = await fetchNext10Days();
-    datesArray.forEach(date => {
-        getSlotsForDate(date);
-    })
+		indices_checked += 1;
+		if( indices_checked === datesArray.length ) {
+			resolve(slots);
+		}
+    	});
+    });
 }
 
 function getSlotsForDate(DATE) {
-    let config = {
-        method: 'get',
-        url: 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=' + PINCODE + '&date=' + DATE,
-        headers: {
-            'accept': 'application/json',
-            'Accept-Language': 'hi_IN'
-        }
-    };
+    const url = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=' + PINCODE + '&date=' + DATE;
 
-    axios(config)
-        .then(function (slots) {
-            let sessions = slots.data.sessions;
-            let validSlots = sessions.filter(slot => slot.min_age_limit <= AGE &&  slot.available_capacity > 0)
-            console.log({date:DATE, validSlots: validSlots.length})
-            if(validSlots.length > 0) {
-                notifyMe(validSlots);
-            }
+	return fetch( url , {
+		headers: {
+            	'Accept': 'application/json',
+            	'Accept-Language': 'hi_IN'
+        	}
+    	}).then(res => res.json())
+	.then(slots => {
+            	let sessions = slots.sessions;
+            	let validSlots = sessions.filter(slot => slot.min_age_limit <= AGE &&  slot.available_capacity > 0)
+            	console.log({date:DATE, validSlots: validSlots.length})
+           	if(validSlots.length > 0) {
+                	// notifyMe(validSlots);
+		}
+		return validSlots;
         })
-        .catch(function (error) {
-            console.log(error);
-        });
+        .catch( error => {
+        	console.error(error);
+        	return [];
+	})
 }
 
-async function
-
-notifyMe(validSlots){
+async function notifyMe(validSlots){
     let slotDetails = JSON.stringify(validSlots, null, '\t');
     notifier.sendEmail(EMAIL, 'VACCINE AVAILABLE', slotDetails, (err, result) => {
         if(err) {
@@ -72,17 +79,29 @@ notifyMe(validSlots){
     })
 };
 
-async function fetchNext10Days(){
+function getDateString(date) {	// DD-MM-YYYY
+	let str = '';
+	if(date.getDate() < 10) str += '0';
+	str += date.getDate();
+	str += '-';
+	if(date.getMonth()+1 < 10) str += '0';
+	str += date.getMonth() + 1;
+	str += '-';
+	str += date.getFullYear();
+	
+	return str;
+}
+
+function fetchNDays(n){
     let dates = [];
-    let today = moment();
-    for(let i = 0 ; i < 10 ; i ++ ){
-        let dateString = today.format('DD-MM-YYYY')
-        dates.push(dateString);
-        today.add(1, 'day');
+    let today = new Date();
+    for(let i = 0 ; i < n ; i ++ ){
+        dates.push( getDateString(today) );
+	today.setDate(today.getDate() + 1);
     }
     return dates;
 }
 
 
 main()
-    .then(() => {console.log('Vaccine availability checker started.');});
+//    .then(() => {console.log('Vaccine availability checker started.');});
